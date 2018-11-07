c33cddf2845d23518a8c45e0276179d1cd289e43
zhengpanone <zhengpanonehotmail.com>
2018-11-07 13:41:33 +0800

    rewrite conf.py

--- mkdocs/source/conf.py
+++ mkdocs/source/conf.py
@@ -57,7 +57,12 @@ release = '1.0.0'
 
 exclude_patterns = []
 
-html_theme = 'default'
+import sphinx_rtd_theme
+
+#html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
 
